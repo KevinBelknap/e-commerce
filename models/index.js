3a4759cd0170c3d6551f5ff7999c3882984af1ec
Kevin Belknap <kevin.belknap@pepsico.com>
2023-07-20 22:34:07 -0500

    this is my final project

---
+++ models/index.js
@@ -0,0 +1,41 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: 'product_id',
+});
+  Tag.belongsToMany(Product, {
+    through: {
+      model: ProductTag,
+    },
+    foreignKey: 'tag_id',
+  });
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
+
